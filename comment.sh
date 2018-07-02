d8beeebfaf9cfa10aae463710b81d1007d835bdd
hoojo <hoojo@qq.com>
2018-07-02 14:54:57 +0800

    :wrench: changed shell comment sh

--- comment.sh
+++ comment.sh
@@ -4,8 +4,8 @@
 # author:   hoojo
 # email:    hoojo_@126.com
 # github:   https://github.com/hooj0
-# create date: 2018-06-28
-# copyright by hoojo @ 2018
+# create:   2018-06-28
+# copyright by hoojo@2018
 # =================================================================
 #
 # =================================================================
