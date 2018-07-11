8b8aa7638fd011a7878604f4c9fdc6e55f70daf8
hoojo <hoojo@qq.com>
2018-07-11 14:22:09 +0800

    :sparkles: :bento: Added stdin/stdout `cat` shell example

--- 07_samples_file_preview/file_cat.sh
+++ 07_samples_file_preview/file_cat.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+
+
 
 # =================================================================
 #		cat——查看文件内容
