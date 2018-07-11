ed2fa1fe5d43796912c1982b66ae796488c6631a
hoojo <hoojo@qq.com>
2018-07-11 14:25:13 +0800

    :sparkles: :bento: Added file view `tail` shell example

--- 07_samples_file_preview/file_tail.sh
+++ 07_samples_file_preview/file_tail.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+
+
 
 # =================================================================
 #		tail —— 查看最后N行
