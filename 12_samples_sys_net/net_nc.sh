e01cfafbbc5709b7c8746159891610371e0d38f1
hoojo <hoojo@qq.com>
2018-09-11 14:23:20 +0800

    :sparkles: :bento: :recycle:  Changed system net tools `nc` command example

--- 12_samples_sys_net/net_nc.sh
+++ 12_samples_sys_net/net_nc.sh
@@ -4,7 +4,7 @@
 # github:   https://github.com/hooj0
 # create:   2018-09-11
 # copyright by hoojo@2018
-# @changelog Aded system net tools `nc` command example
+# @changelog system net tools `nc` command example
 
 
 
