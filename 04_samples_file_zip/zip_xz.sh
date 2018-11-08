65d781f0cba81479c783a37687184eeda1944787
hoojo <hoojo@qq.com>
2018-11-08 14:13:10 +0800

    :sparkles: :bento: Added file zip `xz` cmd shell example

--- 04_samples_file_zip/zip_xz.sh
+++ 04_samples_file_zip/zip_xz.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-09-20
+# copyright by hoojo@2018
+
+
 
 # =================================================================
 #		xz —— 高比例的文件压缩
