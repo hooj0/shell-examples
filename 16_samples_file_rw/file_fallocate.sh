b11847e0c24bf64aa73ce7b839a7f54218c9e854
hoojo <hoojo@qq.com>
2019-01-04 05:48:55 +0800

    :sparkles: :bento:  Added  file preview `tac` shell command example

--- 16_samples_file_rw/file_fallocate.sh
+++ 16_samples_file_rw/file_fallocate.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+# @changelog Added file preview `tac` shell command example
+
+
 
 # =================================================================
 #		fallocate —— 用于 ext4，xfs，btrf 和 ocfs2 文件系统
