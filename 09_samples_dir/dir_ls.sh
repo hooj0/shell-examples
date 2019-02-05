c0f6dbef4dc955e7f74fb359b550510b2400c51b
hoojo <hoojo@qq.com>
2019-02-05 11:52:55 +0800

    :sparkles: :bento:  Added  dir `ls` shell command example

--- 09_samples_dir/dir_ls.sh
+++ 09_samples_dir/dir_ls.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-10-19
+# copyright by hoojo@2018
+# @changelog Added dir `ls` shell command example
+
+
 
 # =================================================================
 #		ls ——  文件系统 列表
