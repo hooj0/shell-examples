e321593ccc45887300c7d5785f81e8a4f11ad68f
hoojo <hoojo@qq.com>
2019-02-05 11:53:01 +0800

    :sparkles: :bento:  Added  dir `mkdir` shell command example

--- 09_samples_dir/dir_mkdir.sh
+++ 09_samples_dir/dir_mkdir.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-10-19
+# copyright by hoojo@2018
+# @changelog Added dir `mkdir` shell command example
+
+
 
 # =================================================================
 #		mkdir —— 创建文件夹
