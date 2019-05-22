4679f3381b8d0f8716b3b3b442398baa5b3ee288
hoojo <hoojo@qq.com>
2019-05-22 21:23:56 +0800

    :sparkles: :bento:  Added  zip `zgrep` shell command example

--- 11_samples_file_zip/zip_zgrep.sh
+++ 11_samples_file_zip/zip_zgrep.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-09-21
+# copyright by hoojo@2018
+# @changelog Added zip `zgrep` shell command example
+
+
 
 # =================================================================
 #		
