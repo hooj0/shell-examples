75c3612c38eced13cf480041c9aaa2bf1c5c1967
hoojo <hoojo@qq.com>
2019-07-04 15:42:01 +0800

    :sparkles: :bento:  Added  sys net `whois` shell command example

--- 21_samples_sys_net/net_whois.sh
+++ 21_samples_sys_net/net_whois.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-17
+# copyright by hoojo@2018
+# @changelog Added sys net `whois` shell command example
+
+
 
 # =================================================================
 #		whois —— 获取某个域名的 whois 信息
