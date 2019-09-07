7b751a8b44bc876ebcb7c3459aebe10db3f7c866
hoojo <hoojo@qq.com>
2019-09-07 21:44:56 +0800

    :sparkles: :bento:  Added  sys net `dig` shell command example

--- 21_samples_sys_net/net_dig.sh
+++ 21_samples_sys_net/net_dig.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-17
+# copyright by hoojo@2018
+# @changelog Added sys net `dig` shell command example
+
+
 
 # =================================================================
 #		dig —— 输出某个域名的 DNS 信息
