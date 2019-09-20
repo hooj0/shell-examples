b2d99ba3a073e667035f39c16dbe63e91b4720a4
hoojo <hoojo@qq.com>
2019-09-20 18:32:04 +0800

    :sparkles: :bento:  Added  file preview `tac` shell command example

--- 16_samples_file_rw/file_perl.sh
+++ 16_samples_file_rw/file_perl.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+# @changelog Added file preview `tac` shell command example
+
+
 
 # =================================================================
 #		
