3a1479976b8a83c40b39a242198937c109acb52a
hoojo <hoojo@qq.com>
2019-09-23 15:14:56 +0800

    :sparkles: :bento:  Added  file rw `tee` shell command example

--- 16_samples_file_rw/file_tee.sh
+++ 16_samples_file_rw/file_tee.sh
@@ -4,7 +4,7 @@
 # github:   https://github.com/hooj0
 # create:   2018-07-07
 # copyright by hoojo@2018
-# @changelog Added file preview `tac` shell command example
+# @changelog Added file rw `tee` shell command example
 
 
 
