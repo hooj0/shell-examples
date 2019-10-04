b50c0c6abc092b705e486e6ba4ed2f31ca711801
hoojo <hoojo@qq.com>
2019-10-05 05:41:55 +0800

    :sparkles: :bento:  Added  sys `lsb_release` shell command example

--- 17_samples_sys/sys_lsb.sh
+++ 17_samples_sys/sys_lsb.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+# @changelog Added sys `lsb_release` shell command example
+
+
 
 # =================================================================
 #		lsb_release —— 查看系统信息
