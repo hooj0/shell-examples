109ff155dfc13b853b2c30fb31dfff0faeeb1e69
hoojo <hoojo@qq.com>
2019-10-13 12:45:02 +0800

    :sparkles: :bento:  Added  file preview `tac` shell command example

--- 16_samples_file_rw/file_xfs_mkfile.sh
+++ 16_samples_file_rw/file_xfs_mkfile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+# @changelog Added file preview `tac` shell command example
+
+
 
 # =================================================================
 #		xfs_mkfile —— 适用于几乎所有的文件系统，包含在 xfsprogs 包
