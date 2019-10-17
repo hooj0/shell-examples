a2e139a37b22cdfda8d1554a3057d43b83eda46f
hoojo <hoojo@qq.com>
2019-10-17 22:55:56 +0800

    :sparkles: :bento:  Added  file preview `tac` shell command example

--- 15_samples_file_preview/file_tac.sh
+++ 15_samples_file_preview/file_tac.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-09
+# copyright by hoojo@2018
+# @changelog Added file preview `tac` shell command example
+
+
 
 # =================================================================
 #		tac —— 反向输出，从后往前输出内容
