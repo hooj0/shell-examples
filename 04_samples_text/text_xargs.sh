8d0d0d2672dbe7d1bfd97f9ca3c5bfe7cc3117ab
hoojo <hoojo@qq.com>
2019-10-21 15:20:04 +0800

    :sparkles: :bento:  Added  text `xargs` shell command example

--- 04_samples_text/text_xargs.sh
+++ 04_samples_text/text_xargs.sh
@@ -4,6 +4,8 @@
 # github:   https://github.com/hooj0
 # create:   2018-09-18
 # copyright by hoojo@2018
+# @changelog Added text `xargs` shell command example
+
 
 
 # =================================================================
