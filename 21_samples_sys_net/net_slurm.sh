8f91355c2871293856b96361fda93ca13529655c
hoojo <hoojo@qq.com>
2019-12-08 23:02:56 +0800

    :sparkles: :bento:  Added  sys net `slurm` shell command example

--- 21_samples_sys_net/net_slurm.sh
+++ 21_samples_sys_net/net_slurm.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# author:   hoojo
+# email:    hoojo_@126.com
+# github:   https://github.com/hooj0
+# create:   2018-07-17
+# copyright by hoojo@2018
+# @changelog Added sys net `slurm` shell command example
+
+
 
 # =================================================================
 #		slurm —— 网络流量可视化
