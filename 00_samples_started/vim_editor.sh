8b698fcfa0d194eb8e2a669de6fdd4ad0d5ed7a1
hoojo <hoojo@qq.com>
2020-11-27 10:11:59 +0800

    :sparkles: :bento: Added started `vim` editor example

--- 00_samples_started/vim_editor.sh
+++ 00_samples_started/vim_editor.sh
@@ -74,16 +74,17 @@
 # 启动Vim
 # ======================================================================================
 vim -c cmd file     在打开文件前，先执行指定的命令
-vim -r file         恢复上次异常退出的文件；
+vim -r file         恢复上次异常退出的文件
 vim -R file         以只读的方式打开文件，但可以强制保存
 vim -M file         以只读的方式打开文件，不可以强制保存
-vim -y num file     将编辑窗口的大小设为num行
+vim -y num file     将编辑窗口的大小设为 num 行
 vim + file          从文件的末尾开始
 vim +num file       从第num行开始
-vim +/string file   打开file，并将光标停留在第一个找到的string上
-vim --remote file   用已有的vim进程打开指定的文件,  如果你不想启用多个vim会话，这个很有用
-                    但要注意， 如果你用vim，会寻找名叫VIM的服务器；如果你已经有一个gvim在运行了
-                    你可以用gvim --remote file在已有的gvim中打开文件
+vim +/string file   打开 file，并将光标停留在第一个找到的 string 上
+vim --remote file   用已有的 vim 进程打开指定的文件,  如果你不想启用多个 vim 会话，这个很有用
+                    但要注意， 如果你用 vim 会寻找名叫 VIM 的服务器；
+                    如果你已经有一个 gvim 在运行了
+                    你可以用 gvim --remote file 在已有的 gvim 中打开文件
 
 
 # 文档操作
@@ -92,7 +93,7 @@ vim --remote file   用已有的vim进程打开指定的文件,  如果你不想
 :e! file    放弃对当前文件的修改，编辑新的文件
 :e+file     开始新的文件，并从文件尾开始编辑
 :e+n file   开始新的文件，并从第n行开始编辑
-:enew       编译一个未命名的新文档。(CTRL-W n)
+:enew       编译一个未命名的新文档(CTRL-W n)
 :e          重新加载当前文档
 :e!         重新加载当前文档，并丢弃已做的改动
 gf          打开以光标所在字符串为文件名的文件
@@ -109,9 +110,9 @@ ZZ              保存并退出
 :q[uit]         退出当前窗口(CTRL-W q 或 CTRL-W CTRL-Q)
 :saveas newfile 另存为
 
-:browse e   会打开一个文件浏览器让你选择要编辑的文件。 如果是终端中，则会打开netrw的文件浏览窗口；
+:browse e   会打开一个文件浏览器让你选择要编辑的文件。 如果是终端中，则会打开netrw的文件浏览窗口
             如果是 gvim，则会打开一个图形界面的浏览窗口
-            实际上 :browse 后可以跟任何编辑文档的命令，如 sp 等。
+            实际上 :browse 后可以跟任何编辑文档的命令，如 sp 等
 
 # 用browse打开的起始目录可以由browsedir来设置：
 :set browsedir=last     用上次访问过的目录（默认）
